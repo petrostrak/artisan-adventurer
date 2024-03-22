7a9fb4f1f4e833000fcff0bddbbfe772cff222cb
petrostrak <pit.trak@gmail.com>
2024-03-22 19:04:31 +0200

    ADD SDLResizeTexture and SDLUpdateWindow

--- main.cpp
+++ main.cpp
@@ -3,6 +3,52 @@
 //
 
 #include "SDL2/SDL.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable SDL_Texture *Texture;
+global_variable void *Pixels;
+global_variable int TextureWidth;
+
+internal void
+SDLResizeTexture(SDL_Renderer *Renderer, int Width, int Height)
+{
+    if(Pixels)
+    {
+        free(Pixels);
+    }
+    if(Texture)
+    {
+        SDL_DestroyTexture(Texture);
+    }
+    Texture = SDL_CreateTexture(Renderer,
+                                SDL_PIXELFORMAT_ABGR8888,
+                                SDL_TEXTUREACCESS_STREAMING,
+                                Width,
+                                Height);
+    TextureWidth = Width;
+    Pixels = malloc(Width * Height * 4);
+}
+
+internal void
+SDLUpdateWindow(SDL_Window *Window, SDL_Renderer *Renderer)
+{
+    SDL_UpdateTexture(Texture,
+                      0,
+                      Pixels,
+                      TextureWidth + 4);
+
+    SDL_RenderCopy(Renderer,
+                   Texture,
+                   0,
+                   0);
+
+    SDL_RenderPresent(Renderer);
+}
 
 bool HandleEvent(SDL_Event *Event)
 {
@@ -14,29 +60,30 @@ bool HandleEvent(SDL_Event *Event)
             printf("SDL_QUIT\n");
             ShouldQuit = true;
         }
+            break;
+
         case SDL_WINDOWEVENT:
         {
             switch (Event->window.event) {
-                case SDL_WINDOWEVENT_RESIZED:
+                case SDL_WINDOWEVENT_SIZE_CHANGED:
                 {
-                    printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", Event->window.data1, Event->window.data2);
+                    SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
+                    SDL_Renderer  *Renderer = SDL_GetRenderer(Window);
+                    printf("SDL_WINDOWEVENT_SIZE_CHANGED (%d, %d)\n", Event->window.data1, Event->window.data2);
+                    SDLResizeTexture(Renderer, Event->window.data1, Event->window.data2);
                 }
                     break;
+
+                case SDL_WINDOWEVENT_FOCUS_GAINED:
+                {
+                    printf("SDL_WINDOWEVENT_FOCUS_GAINED\n");
+                }
+
                 case SDL_WINDOWEVENT_EXPOSED:
                 {
                     SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
                     SDL_Renderer *Renderer = SDL_GetRenderer(Window);
-                    static bool IsWhite = true;
-                    if (IsWhite)
-                    {
-                        SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
-                        IsWhite = false;
-                    }
-                    else
-                    {
-                        SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255);
-                        IsWhite = true;
-                    }
+                    SDLUpdateWindow(Window, Renderer);
                 }
                     break;
             }
@@ -54,8 +101,7 @@ int main()
         return SDL_SetError("Failed to initialize SDL\n");
     }
 
-    SDL_Window *Window;
-    Window = SDL_CreateWindow("Artisan Adventurer",
+    SDL_Window *Window = SDL_CreateWindow("Artisan Adventurer",
                               SDL_WINDOWPOS_UNDEFINED,
                               SDL_WINDOWPOS_UNDEFINED,
                               640,
